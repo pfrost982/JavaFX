162ca126bf56a0c00f0a9318f9422d76fd2a9c7a
pfrost982 <power982@gmail.com>
2020-12-18 19:00:02 +0700

    заработал учебный пример и SceneBuilder

--- src/visual_lesson3/Controller.java
+++ src/visual_lesson3/Controller.java
@@ -1,4 +1,31 @@
 package visual_lesson3;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    void buttonPressed() {
+        String input = textField.getText();
+        if (!input.isBlank()) {
+            try {
+                int number = Integer.parseInt(input);
+                label.setText(String.valueOf(number));
+            } catch (NumberFormatException e) {
+                //label.setText("Ошибка! Введите число");
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Введите число");
+                alert.show();
+            }
+        }
+    }
+
 }
--- src/visual_lesson3/Main.java
+++ src/visual_lesson3/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("visual.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+        primaryStage.setTitle("Visual_lesson3");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 

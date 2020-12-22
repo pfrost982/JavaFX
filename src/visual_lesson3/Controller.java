aeb91e98d559742fa24611de1555882e880523b4
pfrost982 <power982@gmail.com>
2020-12-22 14:48:04 +0700

    Вроде готово

--- src/visual_lesson3/Controller.java
+++ src/visual_lesson3/Controller.java
@@ -5,37 +5,113 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static javafx.stage.Modality.APPLICATION_MODAL;
+
 public class Controller {
+    final private int MAXTRYNUM = 7;
+    private int tryNum;
+    private int rand;
+    private boolean firstTimeFlag;
+
+    //------------------------------------------------------------ main_choice
 
     @FXML
-    private TextField textField;
-
-    @FXML
-    private Label label;
-
-    @FXML
-    void buttonPressed() throws IOException {
-        //inputInt();
-        Parent test = FXMLLoader.load(getClass().getResource("test.fxml"));
-        Stage newWindow = new Stage();
-        newWindow.setTitle("Test Second Window");
-        newWindow.setScene(new Scene(test));
-        newWindow.show();
+    void guessNumber() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("guessNumber.fxml"));
+        Parent guessNumber_fxml = loader.load();
+        Stage guessNumberWindow = new Stage();
+        guessNumberWindow.initModality(APPLICATION_MODAL);
+        guessNumberWindow.setScene(new Scene(guessNumber_fxml));
+        Controller controller = loader.getController();
+        guessNumberWindow.setTitle("Угадай число");
+        controller.firstTimeFlag = true;
+        controller.tryNum = 1;
+        controller.setGuessNumberLabel("Попытка: 1/" + MAXTRYNUM);
+        guessNumberWindow.show();
     }
 
-    void inputInt() {
-        String input = textField.getText();
-        textField.clear();
+    @FXML
+    void guessWord() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("guessWord.fxml"));
+        Parent guessNumber_fxml = loader.load();
+        Stage guessNumberWindow = new Stage();
+        guessNumberWindow.initModality(APPLICATION_MODAL);
+        guessNumberWindow.setScene(new Scene(guessNumber_fxml));
+        Controller controller = loader.getController();
+        guessNumberWindow.setTitle("Угадай слово");
+        controller.firstTimeFlag = true;
+        controller.tryNum = 1;
+        controller.setGuessWordLabel2Label("Попытка: 1/" + MAXTRYNUM);
+        guessNumberWindow.show();
+
+    }
+
+    //----------------------------------------------------------- guessNumber
+
+    @FXML
+    private Label guessNumberLabel1;
+
+    @FXML
+    private Label guessNumberLabel2;
+
+    @FXML
+    private TextField guessNumberTextField;
+
+    @FXML
+    private Button guessNumberButton;
+
+    void setGuessNumberLabel(String s) {
+        guessNumberLabel2.setText(s);
+    }
+
+    @FXML
+    void guessNumberButton() {
+        if (firstTimeFlag) {
+            firstTimeFlag = false;
+            rand = (int) (Math.random() * 100 + 1);
+        }
+        int answ = getInt();
+        if (answ > 0) {
+            tryNum++;
+            if (tryNum > MAXTRYNUM) {
+                guessNumberLabel1.setText("К сожалению у вас закончились попытки...");
+                guessNumberLabel2.setText("Было загадано число:");
+                guessNumberButton.setVisible(false);
+                guessNumberTextField.setText(" " + rand + " ");
+                guessNumberTextField.setEditable(false);
+            } else if (answ > rand) {
+                guessNumberLabel1.setText("Ваше число больше загаданного " + rand);//для отладки
+                guessNumberLabel2.setText("Попытка: " + tryNum + "/" + MAXTRYNUM);
+            } else if (answ < rand) {
+                guessNumberLabel1.setText("Ваше число меньше загаданного " + rand);//для отладки
+                guessNumberLabel2.setText("Попытка: " + tryNum + "/" + MAXTRYNUM);
+            } else if (answ == rand) {
+                guessNumberLabel1.setText("Поздравляем вы угадали!!!");
+                guessNumberButton.setVisible(false);
+                guessNumberTextField.setText(" " + rand + " ");
+                guessNumberTextField.setEditable(false);
+            }
+        }
+
+
+    }
+
+    int getInt() {
+        String input = guessNumberTextField.getText();
+        guessNumberTextField.clear();
         if (!input.isBlank()) {
             try {
                 int number = Integer.parseInt(input);
-                label.setText(String.valueOf(number));
+                return number;
             } catch (NumberFormatException e) {
                 Alert alert = new Alert(Alert.AlertType.ERROR, "Введено не число");
                 alert.show();
@@ -44,12 +120,88 @@ public class Controller {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Поле ввода пустое");
             alert.show();
         }
+        return -1;
+    }
+
+    //----------------------------------------------------------- guessWord
+
+    private String[] words = {"apple", "orange", "lemon", "banana",
+            "apricot", "avocado", "broccoli", "carrot",
+            "cherry", "garlic", "grape", "melon",
+            "leak", "kiwi", "mango", "mushroom", "nut",
+            "olive", "pea", "peanut", "pear", "pepper",
+            "pineapple", "pumpkin", "potato"};
+
+    private String answ;
+
+    @FXML
+    private Button guessWordButton;
+
+    @FXML
+    private TextField guessWordTextField;
+
+    @FXML
+    private Label guessWordLabel2;
+
+    @FXML
+    private Label guessWordLabel1;
+
+    void setGuessWordLabel2Label(String s) {
+        guessWordLabel2.setText(s);
     }
 
     @FXML
-    void secondButtonPressed() {
-        Alert alert = new Alert(Alert.AlertType.ERROR, "Нехер жать эту кнопку");
-        alert.show();
+    void guessWordButton() {
+        if (firstTimeFlag) {
+            firstTimeFlag = false;
+            rand = (int) (Math.random() * words.length);
+        }
+        String word = words[rand];
+        String answ = getWord();
+        if (answ != "") {
+            guessWordLabel1.setText(fillMake(word, answ) + " Отладка: " + word); //для отладки
+            tryNum++;
+            if (tryNum > MAXTRYNUM) {
+                guessWordLabel1.setText("К сожалению у вас закончились попытки...");
+                guessWordLabel2.setText("Было загадано слово:");
+                guessWordButton.setVisible(false);
+                guessWordTextField.setText(" " + word + " ");
+                guessWordTextField.setEditable(false);
+            } else if (answ == word) {
+                guessWordLabel1.setText("Поздравляем вы угадали!!!");
+                guessWordButton.setVisible(false);
+                guessWordTextField.setText(" " + word + " ");
+                guessWordTextField.setEditable(false);
+            } else guessWordLabel2.setText("Попытка: " + tryNum + "/" + MAXTRYNUM);
+        }
+
     }
 
+    String getWord() {
+        String input = guessWordTextField.getText();
+        guessWordTextField.clear();
+        if (!input.isBlank()) {
+            return input;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Поле ввода пустое");
+            alert.show();
+            return "";
+        }
+    }
+
+    String fillMake(String word, String answ) {
+        StringBuilder fill = new StringBuilder(15);
+        int wordLength;
+        int i = 0;
+        if (word.length() > answ.length()) wordLength = answ.length();
+        else wordLength = word.length();
+
+        for (; i < wordLength; i++) {
+            if (word.charAt(i) == answ.charAt(i)) fill.append(word.charAt(i));
+            else fill.append("#");
+        }
+        for (; i < 15; i++) fill.append("#");
+
+        return fill.toString();
+    }
 }
--- src/visual_lesson3/Main.java
+++ src/visual_lesson3/Main.java
@@ -9,13 +9,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
-        primaryStage.setTitle("Visual_lesson3");
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main_choice.fxml"));
+        primaryStage.setTitle("Выбор игры");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
